77ff539d24f2ab5cdf6ef6b2f46d639eeabb959c
KronosCodeur <piraliarris@gmail.com>
2024-12-19 22:51:32 +0000

    Add event routes to the API
    Integrated event routes with authentication middleware. This expands the API's functionality, allowing authorized users to access event-related endpoints.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const authRoutes = require("./routes/authRoutes");
 const todoRoutes = require("./routes/todoRoutes");
 const auth = require("./middleware/authMiddleware");
+const eventRoutes = require("./routes/eventRoutes");
 const app = express();
 app.use(express.json());
 app.use('/api/auth',authRoutes);
 app.use('/api',auth,todoRoutes);
+app.use('/api',auth,eventRoutes);
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Bienvenue dans notre API!' });
 });
