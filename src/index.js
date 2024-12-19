cb64fe3eab0cda1798d5430ef857c76b8a12dca0
KronosCodeur <piraliarris@gmail.com>
2024-12-19 18:22:54 +0000

    Switch to CommonJS and integrate API routes/middleware
    Replaced ES6 imports with CommonJS `require` statements for consistency. Added authentication and todo routes with appropriate middleware. Also introduced a welcome route at the root endpoint.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import express from 'express';
-
+const express = require('express');
+const authRoutes = require("./routes/authRoutes");
+const todoRoutes = require("./routes/todoRoutes");
+const auth = require("./middleware/authMiddleware");
 const app = express();
+app.use(express.json());
+app.use('/api/auth',authRoutes);
+app.use('/api',auth,todoRoutes);
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Bienvenue dans notre API!' });
+});
 
 app.listen(3000,()=> console.log('Server is running on http://localhost:3000'));
